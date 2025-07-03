a8cc188737ed9bb245b0ba00c1c3714282772cf6
govind sreekar garimella <govindsreekar27@gmail.com>
2025-07-03 16:04:14 -0400

    updated app.py

--- backend/app.py
+++ backend/app.py
@@ -1,39 +1,29 @@
-# backend/app.py
-
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
+import os
 from model_handler import get_prediction
 
-# Create Flask App 
-
-app = Flask(__name__)
-
-# Enable CORS 
-
-# make requests to this backend.
+app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
 CORS(app)
 
-# API Endpoint for Prediction 
 @app.route('/api/predict', methods=['POST'])
 def predict():
-    if 'file' not in request.files:
-        return jsonify({'error': 'no file provided'}), 400
-    
-    file = request.files['file']
-    model_name = request.form.get('model', 'resnet18')
-
-    if not file:
-        return jsonify({'error': 'no file selected'}), 400
-
+    if 'file' not in request.files: return jsonify({'error': 'no file provided'}), 400
+    file, model_name = request.files['file'], request.form.get('model', 'resnet18')
+    if not file: return jsonify({'error': 'no file selected'}), 400
     try:
-        img_bytes = file.read()
-        prediction = get_prediction(model_name=model_name, image_bytes=img_bytes)
+        prediction = get_prediction(model_name=model_name, image_bytes=file.read())
         return jsonify({'prediction': prediction})
     except Exception as e:
-
         print(f"An error occurred: {e}")
         return jsonify({'error': 'Error making prediction'}), 500
 
-# Main entry point 
-if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__': app.run(debug=True, port=5001)
