e8b440eb6cd41fc0fb605e2b9e0c97a9cfa66fc5
sreekargovind27 <govindsreekar27@gmail.com>
2025-07-11 20:55:01 -0400

    build file

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+echo "==> Installing Python dependencies..."
+pip install -r requirements.txt
+
+echo "==> Changing to frontend directory..."
+cd frontend
+
+echo "==> Installing frontend dependencies..."
+npm install
+
+echo "==> Building frontend for production..."
+npm run build
+
+echo "==> Build complete!"
